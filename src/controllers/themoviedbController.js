5c0de2abead20f3f2441ba65e213e2e36cecfbf7
Diones Pereira Alves <pereiradiones987@gmail.com>
2022-05-03 13:45:56 -0300

    mudando o envio de status e json

--- src/controllers/themoviedbController.js
+++ src/controllers/themoviedbController.js
@@ -1,10 +1,20 @@
-const res = require("express/lib/response");
-const api = require("../service/api");
+const api = require("../service/api.js");
 
 const getFilmesPopulares = async (req, res) => {
     await api.get(`/movie/popular`).then((response) => {
         console.log(JSON.stringify(response.data));
-        return res.json(response.data);    
+        return res.status(200).json(response.data);  
+    }).catch(() => {
+        return res.status(400).json({ message: "Filmes populares não encontrado!" });
+    });
+}
+
+const getDescobrirFilmes = async (req, res) => {
+    await api.get('/discover/movie').then((response) => {
+        console.log(JSON.stringify(response.data))
+        return res.status(200).json(response.data);  
+    }).catch(() => {
+        return res.status(400).json({ message: "Filmes não encontrado!" });
     });
 }
 
@@ -13,11 +23,15 @@ const getDetalheFilme = async (req, res) => {
 
     await api.get(`/movie/${movie_id}`).then((response) => {
         console.log(JSON.stringify(response.data));
-        return res.json(response.data);
+        return res.status(200).json(response.data);  
+    }).catch(() => {
+        return res.status(400).json({ message: "Filmes não encontrado!" });
     });
 }
 
+
 module.exports = {
     getFilmesPopulares,
-    getDetalheFilme
+    getDetalheFilme,
+    getDescobrirFilmes
 }
--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,7 @@ const themoviedbController = require('../controllers/themoviedbController.js');
 const router = Router();
 
 router.get('/filmes/populares', themoviedbController.getFilmesPopulares);
+router.get('/filmes/discover', themoviedbController.getDescobrirFilmes);
 router.get('/filme/detalhes/:movieId', themoviedbController.getDetalheFilme);
 
 module.exports = router;
