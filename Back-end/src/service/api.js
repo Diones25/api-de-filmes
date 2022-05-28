5637c005500b637c3fb93e76463368a9a74ebd8d
Diones Pereira Alves <pereiradiones987@gmail.com>
2022-05-27 21:13:46 -0300

    Adicionando Navbar

--- Back-end/src/service/api.js
+++ Back-end/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = "https://api.themoviedb.org/3";
-const api_key = "";
+const api_key = "f5a4130ffbf5af6a0dc17e45671898d0";
 const language = "pt-BR"
 
 const api = axios.create({
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import NavBar from './components/NavBar/NavBar.js';
+import Home from './components/pages/Home/Home.js';
 
 
 function App() {
   return (
     <>
-     <h1>TMDB Filmes</h1> 
+      <NavBar />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
