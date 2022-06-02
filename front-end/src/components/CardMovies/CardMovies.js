d64f839b4d306a794823a01557a9952b32d75fb5
Diones Pereira Alves <pereiradiones987@gmail.com>
2022-06-02 00:51:30 -0300

    adicionando barra de progresso circular

--- front-end/src/components/CardMovies/CardMovies.js
+++ front-end/src/components/CardMovies/CardMovies.js
@@ -8,16 +8,21 @@ import Morbius from '../../assets/img/Morbius 1.png'
 const CardMovies = () => {
   return (
     <>
-      <Card id="card">
+      <Card id="card" className="mb-5">
         <Card.Img id="cardImg" variant="top" src={Morbius} />
         <Card.Body id="cardBody">
+          <div class="progress-circle over50 p70">
+            <span class="number">66</span>
+            <span class="percent">%</span>
+            <div class="left-half-clipper">
+              <div class="first50-bar"></div>
+              <div class="value-bar"></div>
+            </div>
+          </div>
           <Card.Title>Morbius</Card.Title>
-          <Card.Text>
-            30 de mar de 2022
-          </Card.Text>
+          <Card.Text>30 de mar de 2022</Card.Text>
         </Card.Body>
       </Card>
-      <br />      
     </>
   );
 };
